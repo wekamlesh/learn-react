bc6702fd1d4cbb3ad2a444b422870fdbec059d09
wekamlesh <merugukamlesh@gmail.com>
2020-07-22 12:33:47 +0530

    assigment-02
    Assigment 02
    The list iteration with map method
    rendering output to page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+<<<<<<< Updated upstream
 function App() {
   return (
     <div className="App">
@@ -21,6 +22,57 @@ function App() {
       </header>
     </div>
   );
+=======
+import Validation from "./Validation/Validation";
+import Char from "./Char/Char";
+
+class App extends React.Component {
+  state = {
+    userInput: "",
+  };
+
+  inputChangedHandler = (e) => {
+    this.setState({
+      userInput: e.target.value,
+    });
+  };
+
+  deleteCharHandler = (index) => {
+    const text = this.state.userInput.split("");
+    text.splice(index, 1);
+    const updatedText = text.join("");
+    this.setState({
+      userInput: updatedText,
+    });
+  };
+
+  render() {
+    const charList = this.state.userInput.split("").map((ch, index) => {
+      return (
+        <Char
+          character={ch}
+          key={index}
+          clicked={() => this.deleteCharHandler(index)}
+        />
+      );
+    });
+
+    return (
+      <div className="App">
+        <h1>It's Working!</h1>
+
+        <input
+          type="text"
+          onChange={this.inputChangedHandler}
+          value={this.state.userInput}
+        />
+        <p>userInput:{this.state.userInput}</p>
+        <Validation inputLength={this.state.userInput.length} />
+        {charList}
+      </div>
+    );
+  }
+>>>>>>> Stashed changes
 }
 
 export default App;
---
+++ src/Char/Char.js
@@ -0,0 +1,18 @@
+/* eslint-disable react/react-in-jsx-scope */
+import React from "react";
+
+const char = (props) => {
+  const style = {
+    display: "inline-block",
+    padding: "16px",
+    margin: "16px",
+    border: "1px solid black",
+  };
+  return (
+    <div style={style} onClick={props.clicked}>
+      {props.character}
+    </div>
+  );
+};
+
+export default char;
